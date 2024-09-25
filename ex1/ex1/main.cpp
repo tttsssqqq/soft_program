3d3ff7f3104a9240f585ca836a4649acab5372cc
tsq2022 <834283702@qq.com>
2024-09-25 17:42:25 +0800

    add 1 C++ program

---
+++ ex1/ex1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ex1
+//
+//  Created by 汤舒琪 on 2024/9/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
